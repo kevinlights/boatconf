947bac877313d51b63f5d556c6f516c11facf1fc
Remi Rampin <r@remirampin.com>
2019-10-17 00:21:47 -0400

    Have balls arc

--- Ball.gd
+++ Ball.gd
@@ -1,6 +1,9 @@
 extends StaticBody
 
-const SPEED = 0.2
+const SPEED = 0.18
+
+var speed_y = 1.0
 
 func _physics_process(delta):
-    translate_object_local(SPEED * Vector3(0, 0, -1))
+    speed_y -= 0.04
+    translate_object_local(SPEED * Vector3(0, speed_y, -1))
