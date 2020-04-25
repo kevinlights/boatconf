bc9a725f6ffb772a2cd4e4d9547a02d22d208fef
Remi Rampin <r@remirampin.com>
2020-04-25 19:22:39 -0400

    Add fire particle effect

---
+++ Fire.gd
@@ -0,0 +1,20 @@
+extends Spatial
+
+const MAX_PARTICLES = 64
+
+export(float) var badness = 0.0 setget _set_badness
+
+onready var particles = $Particles
+
+func _set_badness(new_badness):
+	badness = clamp(new_badness, 0.0, 1.0)
+	if not particles:
+		return
+	if badness < 0.05:
+		$Particles.emitting = false
+	else:
+		$Particles.emitting = true
+		$Particles.amount = int(MAX_PARTICLES * badness)
+
+func _ready():
+	_set_badness(badness)
