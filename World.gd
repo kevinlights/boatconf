18633f70e1cbbfa99c5ca8636816579c67fe7c5f
Remi Rampin <r@remirampin.com>
2020-05-01 00:05:30 -0400

    Automatic map generation & camera placement

--- World.gd
+++ World.gd
@@ -1,5 +1,8 @@
 extends Node
 
+# This is set on a separate node "PlayerManager" instead of World simply so that
+# it can run while the World is paused
+
 const SPAWN_POINTS = [
 	Vector2(-12, -12),
 	Vector2(7, -12),
@@ -7,6 +10,11 @@ const SPAWN_POINTS = [
 	Vector2(6, 18),
 	Vector2(-11, 12),
 	Vector2(-11, 0),
+]
+# Maps the tiles from their binary order to the order in the meshlib
+const MAP_TILES = [
+	-1, 1, 0, 5, 3, 7, 14, 11,
+	2, 13, 4, 12, 6, 9, 10, 8,
 ]
 const CONTROLS = ["key1", "key2", "joy1", "joy2", "joy3", "joy4"]
 
@@ -37,10 +45,65 @@ func reset():
 	players.clear()
 	controls.clear()
 
-	# TODO: Create map, pick spawn points
+	# Generate random map
+	var open_list = {[0, 0]: true}
+	var water_tiles = {}
+	while len(water_tiles) < 10:
+		# Take one position at random from the open list
+		var keys = open_list.keys()
+		var idx = randi() % len(keys)
+		var pos = keys[idx]
+		open_list.erase(pos)
+
+		# Mark it as water
+		water_tiles[pos] = true
+
+		# Mark neighbors not already water as open
+		for rel in [[-1, 0], [1, 0], [0, -1], [0, 1], [-1, -1], [-1, 1], [1, -1], [1, 1]]:
+			var other = [pos[0] + rel[0], pos[1] + rel[1]]
+			if not water_tiles.has(other):
+				open_list[other] = true
+
+	# Make gridmap
+	var map = $"../GridMap"
+	map.clear()
+	for y in range(-7, 7):
+		for x in range(-7, 7):
+			var idx = 0
+			for i in [[0, 0, 8], [1, 0, 4], [0, 1, 2], [1, 1, 1]]:
+				if not water_tiles.has([x + i[0], y + i[1]]):
+					idx += i[2]
+			map.set_cell_item(x, 0, y, MAP_TILES[idx])
+
+	# Measure map
+	var minx = 999
+	var maxx = -999
+	var miny = 999
+	var maxy = -999
+	for key in water_tiles.keys():
+		minx = min(minx, key[0])
+		maxx = max(maxx, key[0])
+		miny = min(miny, key[1])
+		maxy = max(maxy, key[1])
+
+	# Transform to world coordinates
+	var worldmin = map.transform.basis.xform(map.map_to_world(minx, 0, miny) - Vector3(2.5, 0.0, 2.5))
+	var worldmax = map.transform.basis.xform(map.map_to_world(maxx, 0, maxy) + Vector3(0.5, 0.0, 0.5))
+
+	# Center camera
+	var camera = $"../Camera"
+	camera.translation = Vector3(0.5 * (worldmin.x + worldmax.x), 0, 0.62743 * worldmax.z + 0.37257 * worldmin.z)
+
+	# Move camera back
+	var diff = max(worldmax.z - worldmin.z, 0.75 * (worldmax.x - worldmin.x))
+	camera.translate_object_local(Vector3(0.0, 0.0, 0.627 * diff))
+
+	# TODO: Pick spawn points
 	spawn_points = SPAWN_POINTS
 
 func _process(delta):
+	if Input.is_action_just_pressed("ui_right"):
+		reset()
 	for control in CONTROLS:
 		if Input.is_action_just_pressed("%s_fire" % control):
 			if not controls.has(control):
